0aca39ce8817bd2fa5db949a4ea623612a14da0a
Lennart Braun <lenerd@posteo.de>
2015-11-15 23:48:27 +0100

    blatt 4: 2 bar

--- blatt_04/2_textgenerierung.rkt
+++ blatt_04/2_textgenerierung.rkt
@@ -6,49 +6,61 @@
 ; Jan-Hendrik Briese, Lennart Braun, Felix Gebauer
 ; Aufgabe 2: Textgenerierung
 
-; <notmeldung> ::= <ueberschrift> standortangabe notfall_art hilfeleistung 
-;                <peilzeichen> <unterschrift> <over>
+; Backus-Naur-Form für Notrufe:
+;
+; <notmeldung>       ::= <ueberschrift> <notfall> <hilfeleistung> <peilzeichen>
+;                        <unterschrift> <over>
 ; 
-; <ueberschrift> ::= <3x_mayday> <hier_ist> <3x_schiffsname> <rufzeichen>
-;                    <mayday> <schiffsname> <icao_schiffsname> <rufzeichen>
-; <standortangabe> ::= <string>
-; <notfall_art> ::= <string>
-; <hilfeleistung> ::= <string>
-; <peilzeichen> ::= "ICH SENDE DEN TRAEGER --"
-; <unterschrift> :== <schiffsname> <rufzeichen>
-; <over> ::= "OVER"
+; <ueberschrift>     ::= <3x_mayday> <hier_ist> <3x_schiffsname> <rufzeichen>
+;                        <mayday> <schiffsname> <icao_schiffsname> <rufzeichen>
+; <notfall>          ::= <position> <zeit> <status>
+; <peilzeichen>      ::= "ICH SENDE DEN TRAEGER --"
+; <unterschrift>     ::= <schiffsname> <rufzeichen>
+; <over>             ::= "OVER"
+;
+; <position>         ::= "NOTFALLPOSITION" <string>
+; <zeit>             ::= "NOTFALLZEIT" <string>
+; <status>           ::= <string>
+; <hilfeleistung>    ::= <string>
 ; 
-; <mayday> ::= "MAYDAY"
-; <3x_mayday> ::= <mayday> <mayday> <mayday>
+; <mayday>           ::= "MAYDAY"
+; <3x_mayday>        ::= <mayday> <mayday> <mayday>
 ; 
-; <hier_ist> ::= "HIER IST" | "DELTA ECHO"
+; <hier_ist>         ::= "HIER IST" | "DELTA ECHO"
 ; 
-; <schiffsname> ::= <alpha_string>
+; <schiffsname>      ::= <alpha_string>
 ; <icao_schiffsname> ::= <icao_string>
-; <3x_schiffsname> ::= <schiffsname> <schiffsname> <schiffsname>
+; <3x_schiffsname>   ::= <schiffsname> <schiffsname> <schiffsname>
 ; 
-; <rufzeichen> ::= <icao_letter> <icao_letter> <icao_letter> <icao_letter>
+; <rufzeichen>       ::= <icao_letter> <icao_letter> <icao_letter>
+;                        <icao_letter>
 ; 
 ; 
-; <icao_letter> ::= "Alfa" | "Bravo" | "Charlie" | "Delta" | "Echo" |
-;                   "Foxtrott" | "Golf" | "Hotel" | "India" | "Juliett" |
-;                   "Kilo" | "Lima" | "Mike" | "November" | "Oscar" | "Papa" | 
-;                   "Quebec" | "Romeo" | "Sierra" | "Tango" | "Uniform" |
-;                   "Viktor" | "Whiskey" | "X-ray" | "Yankee" | "Zulu"
+; <icao_letter>      ::= "Alfa" | "Bravo" | "Charlie" | "Delta" | "Echo" |
+;                        "Foxtrott" | "Golf" | "Hotel" | "India" | "Juliett" |
+;                        "Kilo" | "Lima" | "Mike" | "November" | "Oscar" |
+;                        "Papa" | "Quebec" | "Romeo" | "Sierra" | "Tango" |
+;                        "Uniform" | "Viktor" | "Whiskey" | "X-ray" | "Yankee" |
+;                        "Zulu"
 ; 
-; <letter> ::= "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" |
-;              "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" |
-;              "W" | "X" | "Y" | "Z"
-; <digit> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+; <letter>           ::= "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" |
+;                        "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" |
+;                        "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z"
+; <digit>            ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" |
+;                        "9"
 ; 
-; <character> ::= <letter> | <digit> | " "
+; <character>        ::= <letter> | <digit> | " "
 ; 
-; <string> ::= <character> | <character> <string>
-; <alpha_string> ::= <letter> | <letter> <alpha_string>
-; <icao_string> ::= <icao_letter> | <icao_letter> <icao_string>
+; <string>           ::= <character> | <character> <string>
+; <alpha_string>     ::= <letter> | <letter> <alpha_string>
+; <icao_string>      ::= <icao_letter> | <icao_letter> <icao_string>
 
 (require "./3.1_die_internationale_buchstabiertafel.rkt")
 
+; Verbindet Strings mit einem Delimiter
+; Parameter:
+;   * del       Delimiter
+;   * strings   Liste von Strings
 (define (join del . strings)
 
         (define (helper strings)
@@ -57,15 +69,17 @@
 
         (string-append (car strings) (helper (cdr strings))))
 
+; Verbindet string mit Leerzeichen als Delimiter
 (define (space-join . strings)
         (apply join " " strings))
-
-(define (line-join . strings)
-        (apply join "\n" strings))
 
 (define (3x str) (space-join str str str))
 
 
+; Generiert die Überschrift eines Notrufes
+; Parameter:
+;   * schiffsname
+;   * rufzeichen
 (define (ueberschrift schiffsname rufzeichen)
         (space-join (3x "MAYDAY")
                     "HIER IST"
@@ -78,25 +92,43 @@
                     "RUFZEICHEN"
                     (apply space-join (icao-spell rufzeichen))))
 
+; Generiert die Beschreibung des Notfalls
+; Parameter:
+;   * position
+;   * zeit
+;   * status
 (define (notfall position zeit status)
         (space-join "NOTFALLPOSITION" position
                     "NOTFALLZEIT" zeit
                     status))
 
+; Generiert die Unterschrift eines Notrufes
+; Parameter:
+;   * schiffsname
+;   * rufzeichen
 (define (unterschrift schiffsname rufzeichen)
         (space-join schiffsname
                     (apply space-join (icao-spell rufzeichen))))
 
+; Wandelt die Kleinbuchstaben in einem String in Großbuchstaben um.
 (define (str-upper s)
         (list->string (map upper (string->list s))))
 
+; Generiert einen Notruf
+; Parameter:
+;   * schiffsname
+;   * rufzeichen
+;   * position
+;   * zeit
+;   * status
+;   * hilfeleistung
 (define (notruf schiffsname rufzeichen position zeit status hilfeleistung)
         (str-upper (space-join (ueberschrift schiffsname rufzeichen)
-                           (notfall position zeit status)
-                           hilfeleistung
-                           "ICH SENDE DEN TRAEGER --"
-                           (unterschrift schiffsname rufzeichen)
-                           "OVER")))
+                               (notfall position zeit status)
+                               hilfeleistung
+                               "ICH SENDE DEN TRAEGER --"
+                               (unterschrift schiffsname rufzeichen)
+                               "OVER")))
 
 
 (notruf "Babette"  ; Schiffsname
